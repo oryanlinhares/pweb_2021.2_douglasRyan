a1c03b9e602ecabc1f258866affec9e459e59a72
oryanlinhares <douglasryans13@gmail.com>
2022-06-19 22:43:19 -0300

    vf
    Implementação do UserStory 7 e 8

--- unidade2/hedoton/src/main/java/com/pweb_2021_1/u2/hedoton/hedoton/controller/ProdutosController.java
+++ unidade2/hedoton/src/main/java/com/pweb_2021_1/u2/hedoton/hedoton/controller/ProdutosController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.pweb_2021_1.u2.hedoton.hedoton.model.Produtos;
 import com.pweb_2021_1.u2.hedoton.hedoton.repositories.ProdutosRepository;
@@ -37,5 +38,22 @@ public class ProdutosController {
     public String cadastrarProduto(Produtos produto){
         produtoRepo.save(produto);
         return "redirect:/produtos";
+    }
+
+    @PostMapping("/pesquisarProduto")
+    public ModelAndView pesquisar(@RequestParam String search) {
+        ModelAndView mav = new ModelAndView("search");
+        Boolean isNumeric = true;
+        for (int i = 0; i < search.length(); i++) {
+            if (Character.isDigit(search.charAt(i))) {
+                isNumeric = false;
+            }
+        }
+        if (isNumeric == false) {
+            mav.addObject("produto", produtoRepo.findByValue(Float.valueOf(search)));
+        } else{
+            mav.addObject("produto", produtoRepo.findByNome(search));
+        }
+        return mav;
     }
 }
--- unidade2/hedoton/src/main/java/com/pweb_2021_1/u2/hedoton/hedoton/repositories/ProdutosRepository.java
+++ unidade2/hedoton/src/main/java/com/pweb_2021_1/u2/hedoton/hedoton/repositories/ProdutosRepository.java
@@ -2,10 +2,19 @@ package com.pweb_2021_1.u2.hedoton.hedoton.repositories;
 
 import com.pweb_2021_1.u2.hedoton.hedoton.model.Produtos;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 @Repository
 public interface ProdutosRepository extends JpaRepository<Produtos, Long> {
 
+    @Query("SELECT prod FROM Produtos prod WHERE prod.nomeProduto LIKE %:nomeE% OR prod.marca LIKE %:nomeE%")
+    List<Produtos> findByNome(@Param("nomeE") String nome);
+
+    @Query("SELECT prod FROM Produtos prod WHERE prod.peso =:nomeE OR prod.altura =:nomeE OR prod.largura =:nomeE OR prod.profundidade =:nomeE OR prod.volume =:nomeE OR prod.precoCompra =:nomeE OR prod.precoVenda =:nomeE")
+    List<Produtos> findByValue(@Param("nomeE") Float nome);
 }
